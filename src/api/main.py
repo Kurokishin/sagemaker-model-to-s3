c84f7e115d7a348dc2dfb67714b89f9c4604433c
Kurokishin <rafaelpereirasouza15@gmail.com>
2023-01-26 12:03:12 -0300

    Adicionando tratamento de erros

--- src/api/main.py
+++ src/api/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
@@ -8,8 +8,11 @@ def index():
 
 @app.route('/api/v1/predict', methods=["GET", "POST"])
 def predict():
-    if request.method == "GET":
-        return 'Em desenvolvimento'
+    try:
+        if request.method == "GET":
+            return 'Em desenvolvimento'
+    except Exception as e:
+        return jsonify(error=str(e))
 
 if __name__ == "__main__":
     app.run(debug=True)
