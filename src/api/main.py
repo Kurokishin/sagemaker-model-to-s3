fe7d9f95dc7c4a70b2918b775b48816ba2312bb1
Samara Alcantara <samara31oliveira@gmail.com>
2023-01-28 22:00:13 -0300

    atualizando retorno api

--- src/api/main.py
+++ src/api/main.py
@@ -1,6 +1,9 @@
+import numpy as np
 from flask import Flask, request, jsonify
+import json
 import boto3
 import pickle
+import os
 
 app = Flask(__name__)
 
@@ -11,23 +14,31 @@ s3 = boto3.client('s3')
 model_file = s3.get_object(Bucket='bucket_name', Key = 'path/to/model.pkl')['Body']
 
 # Carrega o modelo para a aplicação
-loaded_model = pickle.load(model_file)
+loaded_model = pickle.load(open(model_file, 'rb'))
 
 @app.route('/')
 def index():
-    return 'App do Grupo 4!'
+    return 'App do Grupo 4!', 200
 
 @app.route('/api/v1/predict', methods=["GET", "POST"])
+
+# Definindo parâmetros de retorno e tratamento de erro
 def predict():
     try:
         if request.method == "GET":
             return 'Em desenvolvimento'
         elif request.method == "POST":
             json_data = request.get_json()
-            # fazer algo com o json_data
-            return jsonify(result='Predição feita')
+            predicao = model_file.predict(np.array([list(json_data.values())]))
+            resultado = predicao[0]
+
+            resposta = {"result": int(resultado)}
+           
+            return jsonify(resposta)
+
     except Exception as e:
         return jsonify(error=str(e))
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
     app.run(debug=True)
